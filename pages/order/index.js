97188906a7f1daf590b360014a81d5048857fe4f
hlp909 <1015965397@qq.com>
2020-12-29 15:54:43 +0800

    发起订单支付

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,17 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // 、地址
+    // 地址
     address:wx.getStorageSync("address")||{},
     // 购物车商品
     goods:wx.getStorageSync("goods")||{},
     // 总价格
-    totalPrice:0
+    totalPrice:0,
+    // 总数量
+    totalNumber: 0
   },
 
-  onLoad: function (options){
+  /**
+  * 生命周期函数--监听页面加载
+  */
+  onLoad: function (options) {
     this.handleAllPrice();
   },
+
 
    // 封装计算总价格
   handleAllPrice() {
@@ -36,7 +42,7 @@ Page({
     })
   },
 
-  // 立即支付
+  // 发起支付
   handlePay(){
     const {goods, address, totalPrice}=this.data;
     const newGoods=Object.keys(goods).map(v=>{
@@ -44,6 +50,7 @@ Page({
       return goods[v];
     })
 
+    // 创建订单成功
     request({
       url:"/my/orders/create",
       method:"POST",
@@ -56,7 +63,33 @@ Page({
         Authorization:wx.getStorageSync("token")
       }
     }).then(res=>{
-      console.log(res)
+      // 获取订单编号
+      const {order_number}=res.data.message;
+      // 请求支付的参数
+      request({
+        url:"/my/orders/req_unifiedorder",
+        method:"POST",
+        data:{
+          order_number
+        },
+        header:{
+          Authorization: wx.getStorageSync("token")
+        }
+      }).then(res=>{
+        // pay是对象，包含了所有的支付参数
+        const { pay } = res.data.message;
+
+        // 发起支付,调用微信的原生支付窗口
+        wx.requestPayment({
+          ...pay,
+          success: () => {
+            // 把本地的goods列表中selected为true的商品删除掉
+            let newGoods = wx.getStorageSync("goods")
+            newGoods = newGoods.filter(v=>!v.selected)
+            wx.setStorageSync('goods', newGoods)
+          }
+      })
+      })
     })
   }
 
